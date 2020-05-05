4256ea9a722f8cd5810070983ac9c2ff81ad0813
Ellen Marie Dash <me@duckie.co>
2020-05-05 16:52:53 -0400

    Add build and run scripts.

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MUSIC_DIR="$1"
+
+if [ -z "$1" ] || [ "$1" -eq "-h" ] || [ "$1" == "--help" ]; then
+  echo "Usage: $0 MUSIC_DIR"
+  exit 1
+fi
+
+mkdir -p lib-mpd
+
+docker run --rm -it -v "$MUSIC_DIR:/var/lib/mpd/music:z" -v "$(pwd)/lib-mpd:/var/lib/mpd:z" -p 6600:6600 -p 8000:8000 mpd
